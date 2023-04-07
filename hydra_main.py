2662d100668fc03a992d6d2fa494ec4d6e8c211c
Daniel Zhu <daniel.zhu@imt-atlantique.fr>
2023-04-07 15:32:21 +0200

    Changer la façon de transformer un modèle
    À coup de load_state et de state_dict
    
    Par conséquent, j'affiche aussi le hash dans les tableaux

--- hydra_main.py
+++ hydra_main.py
@@ -109,7 +109,22 @@ class FourDVarNetHydraRunner:
         return mod
 
     def _inject_OI_to_MP(self, mod, ckpt_path):
+        import hashlib
+        import json
+
         from dashtable import data2rst
+
+        def _hash(_model):
+            d = _model.state_dict()
+            for key, value in d.items():
+                if isinstance(value, torch.Tensor):
+                    d[key] = value.cpu().numpy().tolist()
+            return hashlib.sha256(
+                json.dumps(d, sort_keys=True).encode('utf8')
+            ).hexdigest()[:10]
+
+        def _hit(_model):  # hash, id, type
+            return _hash(_model), str(id(_model))[-10:], type(_model)
 
         _mod = self.lit_cls.load_from_checkpoint(
             ckpt_path,
@@ -128,22 +143,26 @@ class FourDVarNetHydraRunner:
 
         print('>>> ckpt_path:', ckpt_path, '\n')
 
-        _headers = ['', 'mod (id)', 'mod (type)', '_mod (id)', '_mod (type)']
+        _headers = [
+            '',
+            'mod (hash)', 'mod (id)', 'mod (type)',
+            '_mod (hash)', '_mod (id)', '_mod (type)'
+        ]
 
         print('>>> 0. General')
         print(data2rst(
             [
                 _headers,
-                ['.', id(mod), type(mod), id(_mod), type(_mod)],
+                ['.', *_hit(mod), *_hit(_mod)],
                 [
                     '.model',
-                    id(mod.model), type(mod.model),
-                    id(_mod.model), type(_mod.model)
+                    *_hit(mod.model),
+                    *_hit(_mod.model)
                 ],
                 [
                     '.model.phi_r',
-                    id(mod.model.phi_r), type(mod.model.phi_r),
-                    id(_mod.model.phi_r), type(_mod.model.phi_r)
+                    *_hit(mod.model.phi_r),
+                    *_hit(_mod.model.phi_r)
                 ],
             ],
             use_headers=True,
@@ -155,43 +174,43 @@ class FourDVarNetHydraRunner:
                 _headers,
                 [
                     '.model.model_H',
-                    id(mod.model.model_H), type(mod.model.model_H),
-                    id(_mod.model.model_H), type(_mod.model.model_H)
+                    *_hit(mod.model.model_H),
+                    *_hit(_mod.model.model_H)
                 ],
                 [
                     '.model.model_Grad',
-                    id(mod.model.model_Grad), type(mod.model.model_Grad),
-                    id(_mod.model.model_Grad), type(_mod.model.model_Grad)
+                    *_hit(mod.model.model_Grad),
+                    *_hit(_mod.model.model_Grad)
                 ],
                 [
                     '.model.model_VarCost',
-                    id(mod.model.model_VarCost), type(mod.model.model_VarCost),
-                    id(_mod.model.model_VarCost), type(_mod.model.model_VarCost)
+                    *_hit(mod.model.model_VarCost),
+                    *_hit(_mod.model.model_VarCost)
                 ],
             ],
             use_headers=True,
         ))
-        mod.model.model_H = _mod.model.model_H
-        mod.model.model_Grad = _mod.model.model_Grad
-        mod.model.model_VarCost = _mod.model.model_VarCost
+        mod.model.model_H.load_state_dict(_mod.model.model_H.state_dict())
+        mod.model.model_Grad.load_state_dict(_mod.model.model_Grad.state_dict())
+        mod.model.model_VarCost.load_state_dict(_mod.model.model_VarCost.state_dict())
         print('>>> after transfer:')
         print(data2rst(
             [
                 _headers,
                 [
                     '.model.model_H',
-                    id(mod.model.model_H), type(mod.model.model_H),
-                    id(_mod.model.model_H), type(_mod.model.model_H)
+                    *_hit(mod.model.model_H),
+                    *_hit(_mod.model.model_H)
                 ],
                 [
                     '.model.model_Grad',
-                    id(mod.model.model_Grad), type(mod.model.model_Grad),
-                    id(_mod.model.model_Grad), type(_mod.model.model_Grad)
+                    *_hit(mod.model.model_Grad),
+                    *_hit(_mod.model.model_Grad)
                 ],
                 [
                     '.model.model_VarCost',
-                    id(mod.model.model_VarCost), type(mod.model.model_VarCost),
-                    id(_mod.model.model_VarCost), type(_mod.model.model_VarCost)
+                    *_hit(mod.model.model_VarCost),
+                    *_hit(_mod.model.model_VarCost)
                 ],
             ],
             use_headers=True,
@@ -205,12 +224,15 @@ class FourDVarNetHydraRunner:
 
             _phis.append([
                 rowlegend,
-                id(cphi), type(cphi),
-                id(_mod.model.phi_r), type(_mod.model.phi_r)
+                *_hit(cphi),
+                *_hit(_mod.model.phi_r)
             ])
 
             # transfer
-            mod.model.phi_r.phi_list[i] = _mod.model.phi_r
+            mod.model.phi_r.phi_list[i].load_state_dict(_mod.model.phi_r.state_dict())
+
+        # Noise last prior's last layer (N(0, 10⁻²))
+        mod.model.phi_r.phi_list[-1].noise_last_layer(0., .01)
 
         print(data2rst(
             [_headers, *_phis],
@@ -225,8 +247,8 @@ class FourDVarNetHydraRunner:
 
             _phis.append([
                 rowlegend,
-                id(cphi), type(cphi),
-                id(_mod.model.phi_r), type(_mod.model.phi_r)
+                *_hit(cphi),
+                *_hit(_mod.model.phi_r)
             ])
 
         print(data2rst(
